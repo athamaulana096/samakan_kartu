17b01bd258b3b8f5e6d06ee21921b0be99470001
athamaulana096 <athamaulana30@gmail.com>
2023-07-05 13:22:03 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,62 @@
+let jumlahKartu = 10;
+let kartuPertama = (kartuKedua = 0);
+
+function buatAngka() {
+  let angkaBerurut = [];
+
+  for (let i = 1; i <= jumlahKartu; i++) {
+    angkaBerurut.push(i, i);
+  }
+
+  return angkaBerurut;
+}
+
+function acakAngka(angkaBerurut) {
+  let angkaAcak = angkaBerurut.sort(function () {
+    return 0.5 - Math.random();
+  });
+
+  return angkaAcak;
+}
+
+function persiapkanKartu(angkaAcak) {
+  let str = '';
+
+  angkaAcak.forEach(function (i) {
+    str += '<div class="kartu" nilai="' + i + '">' + '<div class="belakang">' + i + '</div>' + '<div class="depan">Kartu</div>' + '</div>';
+  });
+
+  $('#game').append(str);
+}
+
+function pengendali() {
+  $('.kartu').on('click', function () {
+    $(this).addClass('buka');
+
+    if (kartuPertama == 0) {
+      kartuPertama = $(this).attr('nilai');
+    } else {
+      kartuKedua = $(this).attr('nilai');
+      if (kartuPertama == kartuKedua) {
+        $('.buka').addClass('benar');
+        $('.benar').removeClass('buka');
+        kartuPertama = kartuKedua = 0;
+      } else {
+        kartuPertama = kartuKedua = 0;
+        $(this).one('transitionend', function () {
+          $('.kartu').removeClass('buka');
+        });
+      }
+    }
+  });
+}
+
+$(document).ready(function () {
+  let angkaBerurut = buatAngka();
+
+  let angkaAcak = acakAngka(angkaBerurut);
+
+  persiapkanKartu(angkaAcak);
+
+  pengendali();
+});
